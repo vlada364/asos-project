4dee096530fe2634a29202d49590742090f0b50b
fake364 <super4prikolist@gmail.com>
2023-01-30 00:02:34 +0200

    some styles changes

--- src/components/Registration/JoinIn.jsx
+++ src/components/Registration/JoinIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './RegistrationForm.module.css'
 import SignUpWith from "./SignUpWith";
+import Form from './Form'
 
 
 const JoinIn = () => {
@@ -8,81 +9,7 @@ const JoinIn = () => {
         <SignUpWith/>
         <div className={styles.registrationForm}>
             <h2>OR SIGN UP WITH EMAIL</h2>
-            <form id="form" className={styles.form}>
-                <label className={styles.nameLabel}> EMAIL ADDRESS:</label>
-                <div id="tooltip"></div>
-                <input id="email_address" className={styles.formInput} value="" type="text"/>
-                <div className={styles.littleNote}><p>We'll send your order confirmation here</p></div>
-                <label className={styles.nameLabel}> FIRST NAME:</label>
-                <div id="tooltip_name"></div>
-                <input id="first_name" className={styles.formInput}
-                       value="" maxLength="15" type="text"/>
-                <label className={styles.nameLabel}> LAST NAME:</label>
-                <div id="tooltip_last_name"></div>
-                <input id="last_name" className={styles.formInput} value=""/>
-                <label className={styles.nameLabel}> PASSWORD:</label>
-                <div id="tooltip_password"></div>
-                <input id="password"
-                       className={styles.formInput} value=""/>
-                <div className={styles.littleNote}><p>Must be 10 or more characters</p></div>
-                <div><label className={styles.nameLabel}>DATE OF
-                    BIRTH:</label></div>
-                <div style={{  display: 'flex',justifyContent: 'space-between'}}>
-                    <select id="day" className={styles.selectBlock} name="day">
-                        <option value="0">DD</option>
-                    </select>
-                    <select id="month" className={styles.selectBlock}  name="month">
-                        <option value="0">Month</option>
-                    </select>
-                    <select id="year" className={styles.selectBlock}  name="year">
-                        <option value="0">YYYY</option>
-                    </select>
-                    {/*<p className="text-gray-400">You need to be 16 or over to use ASOS</p>*/}
-                </div>
-                <div className={styles.nameLabel}>
-                    <p >MOSTLY INTERESTED IN:</p>
-                </div>
-                <div className={styles.selectCategory}>
-                    <div id="women-select" className={styles.radioCheck}>
-                        <input type="radio" name="gender" checked/>
-                        <label>Womenswear</label>
-                    </div>
-                    <div id="men-select" className={styles.radioCheck}>
-                        <input type="radio" name="gender"/>
-                        <label>Menswear</label>
-                    </div>
-                </div>
-                <div  className={styles.contactPreferences}>
-                    <div className="justify-start flex flex-col">
-                        <div className={styles.contactPreferencesText}>
-                            <p>CONTACT PREFERENCES</p>
-                        </div>
-                        <div className={styles.littleNote}>
-                            <p>Tell us which emails you’d like:</p>
-                        </div>
-                    </div>
-                    <div>
-                        <button type="button" className={styles.selectAllBtn}>SELECT ALL</button>
-                    </div>
-                </div>
-                <div id="input_select" className={styles.inputSelectContainer}>
-                    <div className={styles.inputSelectCategory}>
-                        <label>Discounts and sales</label><input type="checkbox" className="checkbox-input"/>
-                    </div>
-                    <div className={styles.inputSelectCategory}>
-                        <label>New stuff</label><input type="checkbox" className="checkbox-input"/>
-                    </div>
-                    <div className={styles.inputSelectCategory}>
-                        <label>Your exclusives</label><input type="checkbox" className="checkbox-input"/>
-                    </div>
-                    <div className={styles.inputSelectCategory}>
-                        <label>Asos partners</label><input type="checkbox" className="checkbox-input"/>
-                    </div>
-                </div>
-                <div className={styles.btnJoinAsos}>
-                    <button type="button" id="btn-asos"><p>JOIN ASOS</p></button>
-                </div>
-            </form>
+           <Form/>
         </div>
     </main>;
 }
--- src/components/Registration/SignIn.jsx
+++ src/components/Registration/SignIn.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
-
+import styles from "./RegistrationForm.module.css";
 
 
 const SignIn = () => {
-    return <div></div>;
+    return (<div className={styles.registrationForm}>
+        <div className={styles.registrationFormTwo}>
+        <form id="form-two" className={styles.form}>
+            <label className={styles.nameLabel}> EMAIL ADDRESS:</label>
+            <div id="tooltip"></div>
+            <input id="last_name" className={styles.formInput} value=""/>
+            <label className={styles.nameLabel}> PASSWORD:</label>
+            <div id="tooltip_password"></div>
+            <input id="password" className={styles.formInput} value=""/>
+            <div className={styles.btnJoinAsos}>
+                <button type="button" id="btn-asos"><p>SIGN IN</p></button>
+            </div>
+            <div className={styles.isForgotPassword}><p>Forgot password?</p></div>
+        </form>
+            <h2>OR SIGN IN WITH...</h2>
+            <div className={styles.buttonsContainer}>
+                <div className={styles.buttons}>
+                    <div className={styles.buttonSize}><img src=".//img/img.png"/></div>
+                    <div>
+                        <button id="google"><p>GOOGLE</p></button>
+                    </div>
+                </div>
+
+                <div className={styles.buttons}>
+                    <div className={styles.buttonSize}><img src="../img/img_1.png"/></div>
+                    <div>
+                        <button id="apple"><p >APPLE</p></button>
+                    </div>
+                </div>
+                <div className={styles.buttons}>
+                    <div className={styles.buttonSize}><img src="../img/img_2.png"/></div>
+                    <div>
+                        <button id="facebook "><p>FACEBOOK</p></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>);
 }
 
 export default SignIn;
