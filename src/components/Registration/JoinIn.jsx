4c53c055ffdfbfc0f911d09792704993f03acbee
serebreanuv <valentin.serebreanu@reedexpo.co.uk>
2023-01-29 19:04:52 +0000

    changed join in page

--- src/components/Registration/JoinIn.jsx
+++ src/components/Registration/JoinIn.jsx
@@ -12,7 +12,7 @@ const JoinIn = () => {
                 <label className={styles.nameLabel}> EMAIL ADDRESS:</label>
                 <div id="tooltip"></div>
                 <input id="email_address" className={styles.formInput} value="" type="text"/>
-                <div className={}><p>We'll send your order confirmation here</p></div>
+                <div className={styles.littleNote}><p>We'll send your order confirmation here</p></div>
                 <label className={styles.nameLabel}> FIRST NAME:</label>
                 <div id="tooltip_name"></div>
                 <input id="first_name" className={styles.formInput}
@@ -24,63 +24,62 @@ const JoinIn = () => {
                 <div id="tooltip_password"></div>
                 <input id="password"
                        className={styles.formInput} value=""/>
-                <div><p>Must be 10 or more characters</p></div>
+                <div className={styles.littleNote}><p>Must be 10 or more characters</p></div>
                 <div><label className={styles.nameLabel}>DATE OF
                     BIRTH:</label></div>
-                <div>
-                    <select id="day" className="py-2.5 pl-3 pr-3 border-[2px] border-solid" name="day">
+                <div style={{  display: 'flex',justifyContent: 'space-between'}}>
+                    <select id="day" className={styles.selectBlock} name="day">
                         <option value="0">DD</option>
                     </select>
-                    <select id="month" className="py-2.5 pl-3 pr-3 border-[2px] border-solid" name="month">
+                    <select id="month" className={styles.selectBlock}  name="month">
                         <option value="0">Month</option>
                     </select>
-                    <select id="year" className="py-2.5 pl-3 pr-3 border-[2px] border-solid" name="year">
+                    <select id="year" className={styles.selectBlock}  name="year">
                         <option value="0">YYYY</option>
                     </select>
-                    <p className="text-gray-400">You need to be 16 or over to use ASOS</p>
+                    {/*<p className="text-gray-400">You need to be 16 or over to use ASOS</p>*/}
                 </div>
                 <div className={styles.nameLabel}>
-                    <p >MOSTLY INTERESTED IN</p>
+                    <p >MOSTLY INTERESTED IN:</p>
                 </div>
-                <div className=" select-category flex  gap-3 justify-between">
-                    <div id="women-select" className="flex justify-between gap-5">
+                <div className={styles.selectCategory}>
+                    <div id="women-select" className={styles.radioCheck}>
                         <input type="radio" name="gender" checked/>
                         <label>Womenswear</label>
                     </div>
-                    <div id="men-select" className="flex justify-between gap-5">
+                    <div id="men-select" className={styles.radioCheck}>
                         <input type="radio" name="gender"/>
                         <label>Menswear</label>
                     </div>
-
                 </div>
-                <div className="flex flex-row justify-start gap-6">
+                <div  className={styles.contactPreferences}>
                     <div className="justify-start flex flex-col">
-                        <div className="justify-start flex">
+                        <div className={styles.contactPreferencesText}>
                             <p>CONTACT PREFERENCES</p>
                         </div>
-                        <div>
+                        <div className={styles.littleNote}>
                             <p>Tell us which emails you’d like:</p>
                         </div>
                     </div>
-                    <div className=" border-[2px] py-2.5 pl-3 pr-3 bg-neutral-300">
-                        <button type="button" className="select_all_btn select-none">SELECT ALL</button>
+                    <div>
+                        <button type="button" className={styles.selectAllBtn}>SELECT ALL</button>
                     </div>
                 </div>
-                <div id="input_select" className="flex flex-col justify-around">
-                    <div className="flex flex-row justify-between">
+                <div id="input_select" className={styles.inputSelectContainer}>
+                    <div className={styles.inputSelectCategory}>
                         <label>Discounts and sales</label><input type="checkbox" className="checkbox-input"/>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className={styles.inputSelectCategory}>
                         <label>New stuff</label><input type="checkbox" className="checkbox-input"/>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className={styles.inputSelectCategory}>
                         <label>Your exclusives</label><input type="checkbox" className="checkbox-input"/>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className={styles.inputSelectCategory}>
                         <label>Asos partners</label><input type="checkbox" className="checkbox-input"/>
                     </div>
                 </div>
-                <div className="btn-join pr-20  pl-4 pt-5 pb-5 border-[2px] border-solid text-center">
+                <div className={styles.btnJoinAsos}>
                     <button type="button" id="btn-asos"><p>JOIN ASOS</p></button>
                 </div>
             </form>
